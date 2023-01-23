aa5b6f109f1e0045450b560d2eaf8546da2bad11
sapirmachluf <mahluf.sapir@gmail.com>
2023-01-23 23:06:46 +0200

    MEDIA_ROOT commit

--- backend/settings.py
+++ backend/settings.py
@@ -175,8 +175,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL='/images/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR ,"media")
+MEDIA_ROOT='static/images'
+# MEDIA_ROOT = os.path.join(BASE_DIR ,"media")
 
 STATICFILES_DIRS = [
     BASE_DIR/'static',
