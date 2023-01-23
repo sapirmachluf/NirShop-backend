47c4b5e82ac2ebc923a808c04c0ce969a3cb1798
sapirmachluf <mahluf.sapir@gmail.com>
2023-01-23 22:12:29 +0200

    wsgi commit

--- backend/settings.py
+++ backend/settings.py
@@ -172,9 +172,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+
+STATIC_URL = 'static/'
+MEDIA_URL='/images/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR ,"media")
 
 STATICFILES_DIRS = [
